a7916768ff5ff7de53064b16acccb9aee439734f
fuduhui <fuduhui163@163.com>
2019-03-29 17:07:19 +0800

    add: format date

--- src/index.js
+++ src/index.js
@@ -1,22 +1,44 @@
 Component({
   properties: {
-    prop: {
-      type: String,
-      value: 'index.properties'
-    },
+
   },
   data: {
-    flag: false,
+    weekday: ['一', '二', '三', '四', '五', '六', '日']
   },
   lifetimes: {
     attached() {
-      wx.getSystemInfo({
-        success: () => {
-          this.setData({
-            flag: true,
-          })
-        }
-      })
+    }
+  },
+  methods: {
+    _getMonthDays(year, month) {
+      return new Date(year, month + 1, 0).getDate()
+    },
+
+    _getWeekDay(year, month, day) {
+      return new Date(year, month, day).getDate + 1
+    },
+
+    _tf(i) {
+      i = (typeof i == 'string' ? parseInt(i) : i);
+      return (i < 10 ? '0' : '') + i
+    },
+
+    _formatDate(startStamp, format) {
+      const dateObj = new Date(startStamp)
+      const year = _tf(dateObj.getFullYear())
+      const month = _tf(dateObj.getMonth() + 1)
+      const date = _tf(dateObj.getDate())
+
+      switch(format) {
+        case 'YYYY-MM':
+          return `${year}-${month}`
+        default:
+          return `${year}-${month}-${date}`
+      }
+    },
+    _initStartEnd() {
+      const startStamp = new Date().getTime()
+
     }
   }
 })
